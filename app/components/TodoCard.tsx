4e40ec4454c24da9c6266a8ce2b56bf6a8dfefab
h662 <h662hong@gmail.com>
2024-01-30 11:22:00 +0900

    isDone todo

--- app/components/TodoCard.tsx
+++ app/components/TodoCard.tsx
@@ -1,14 +1,41 @@
 import { Todo } from "@prisma/client";
-import { FC } from "react";
+import axios from "axios";
+import { FC, useState } from "react";
 
 interface TodoCardProps {
   todo: Todo;
 }
 
 const TodoCard: FC<TodoCardProps> = ({ todo }) => {
+  const [isDone, setIsDone] = useState<boolean>(todo.isDone);
+
+  const onIsDoneTodo = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) return;
+
+      await axios.put<Todo>(
+        `${process.env.NEXT_PUBLIC_URL}/api/todo/${todo.id}/is-done`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setIsDone(!isDone);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <li className="mx-8 text-right">
-      <button>{todo.content}</button>{" "}
+      <button className={`${isDone && "line-through"}`} onClick={onIsDoneTodo}>
+        {todo.content}
+      </button>{" "}
       <button className="btn-style">수정</button>{" "}
       <button className="btn-style">삭제</button>
     </li>
